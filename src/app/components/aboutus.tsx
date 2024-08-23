44156433af2a832730277579b56bf15dc3e30c3f
Shahid7424 <shahshahid121212@gmail.com>
2024-08-23 00:18:46 -0700

    add section

---
+++ src/app/components/aboutus.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { AboutImage } from "../utilis/images"
+
+const AboutUs = () => {
+  return (
+    <div></div>
+    // <div className="container mx-auto px-4 py-8 lg:px-24 lg:py-16">
+    //   <div className="flex flex-wrap lg:flex-nowrap">
+    //     {/* Left Column: Text Section */}
+    //     <div className="w-full lg:w-5/12 p-5">
+    //       <div className="text-white">
+    //         <h1 className="text-lg font-medium">About Us</h1>
+    //         <h1 className="text-2xl lg:text-4xl mt-4">The Digital Cybsec and Transform Billboard</h1>
+    //         <p className="mt-3">
+    //           Established in 2016, REXTON Advertising has become one of India's leading suppliers of LED Displays, LED Signages, and LED Mobile Vans. With over 8,000 square feet of LED screens installed across the country, we are dedicated to delivering cutting-edge solutions tailored to our customers' needs.
+    //         </p>
+    //         <p className="mt-3">
+    //           Our advanced technology ensures that our products are quick to install, easy to use, and portable. We cater to a diverse range of industries, including malls, retail chains, and hotels, offering a variety of LED displays such as:
+    //         </p>
+    //         <ul className="list-disc list-inside mt-3">
+    //           <li>Outdoor LED Displays</li>
+    //           <li>Indoor LED Displays</li>
+    //         </ul>
+    //         <div className="mt-4">
+    //           <Link href="#" className="btn bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 inline-flex items-center">
+    //             Learn More <i className="ml-2 icon icon-right-arrow"></i>
+    //           </Link>
+    //         </div>
+    //       </div>
+    //     </div>
+
+    //     {/* Right Column: Image and Stats Section */}
+    //     <div className="w-full lg:w-6/12 lg:ml-8 mt-8 lg:mt-0">
+    //       <div className="relative">
+    //         <Image src={AboutImage} alt="Main Billboard Image" className="w-full h-auto rounded shadow-lg" />
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+  );
+};
+
+export default AboutUs;
---
+++ src/app/utilis/images/index.ts
@@ -0,0 +1,25 @@
+import BannerImage from "./banner-Img.png"
+import AboutImage from "./About-image.jpg"
+import FeatureImg1 from "./out-door-billboard.jpg"
+import FeatureImg2 from "./indoor.jpg"
+import FeatureImg3 from "./digital.jpg"
+import WhyChooseUsImg from "./why.chose-img.png"
+import GalleryImg1 from "./blogimg1.jpg"
+import GalleryImg2 from "./indoorbillboard-4 (1).jpg"
+import GalleryImg3 from "./blogimg5.jpg"
+import GalleryImg4 from "./Led-sign2.jpg"
+import GalleryImg5 from "./advertisement-display-boards.jpeg"
+export{
+    BannerImage,
+    AboutImage,
+    FeatureImg1,
+    FeatureImg2,
+    FeatureImg3,
+    WhyChooseUsImg,
+    GalleryImg1,
+    GalleryImg2,
+    GalleryImg3,
+    GalleryImg4,
+    GalleryImg5
+
+}
